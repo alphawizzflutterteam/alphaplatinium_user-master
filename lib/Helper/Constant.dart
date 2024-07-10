696030dbec6900d9edd358b6f6fe73e2a47690fe
Alphawizz Flutter Team <alphawizzflutterteam@gmail.com>
2024-07-10 16:03:05 +0530

    first commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,23 @@
+final String appName = 'AlphaPlatinum';
+
+final String packageName = 'com.alphaPlatinum.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.alphaPlatinum.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = 'com.alphaPlatinum.user';
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+ final String baseUrl = 'https://alphasilver.productsalphawizz.com/app/v1/api/';
+
+// final String baseUrl = 'https://developmentalphawizz.com/farmbuz/app/v1/api/';
+
+ final String imageUrl = 'https://alphasilver.productsalphawizz.com/';
+// final String imageUrl = 'https://developmentalphawizz.com/farmbuz/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
